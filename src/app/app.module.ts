a3cd4551f9c02452fc6ae7205296d0322a1b2846
[seanchen525] <[chenxiaoyong525@hotmail.com]>
2018-03-20 18:16:09 -0400

    add heros

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent
   ],
   imports: [
     BrowserModule,
