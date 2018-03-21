676bce78cc753c1a8efd625b43f7934e52a2c982
Xiaoyong Chen <Xiaoyong Chen>
2018-03-20 22:53:17 -0400

    hero edit, displaying a list

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROS } from '../mock-heros';
 
 @Component({
   selector: 'app-heros',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heros.component.css']
 })
 export class HerosComponent implements OnInit {
+  heros = HEROS;
+  selectedHero: Hero;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
   }
 
 }
