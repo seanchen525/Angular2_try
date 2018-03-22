7fe6ad2292a823b326049b4389757b1d30092182
[seanchen525] <[chenxiaoyong525@hotmail.com]>
2018-03-22 13:26:45 -0400

    finish tutor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -76,4 +76,14 @@ export class HeroService {
     );
   }
 
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()){
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap(_ => this.log(`search heroes matching "${term}"`)),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
+
 }
